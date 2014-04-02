a5b8c92abf66d33701a39a2dc41772768bed7784
jerry_laptop <jiajiewu1988@gmail.com>
2014-04-01 17:36:06 -0700

    1.8

--- CTCI_Practices/src/chapter/one/Q1_8.java
+++ CTCI_Practices/src/chapter/one/Q1_8.java
@@ -1,5 +1,24 @@
 package chapter.one;
 
+/**
+ * Assume you have the method isSubstring which checks if one word is a substring of another.
+ * Given 2 strings s1 and s2. Write code to check if s2 is a rotation of s1 using only one 
+ * call of isSubstring
+ * @author jerry
+ *
+ *Keys: If s2 is a rotation of s1, then s2 is a substring of s1+s1
+ */
+
 public class Q1_8 {
 
+	public static boolean isRotation(String s1, String s2) {
+		String s = s1 + s1;
+		if (isSubstring(s, s2)) return true;
+		return false;
+	}
+	
+	public static boolean isSubstring(String s1, String s2) {
+		if (s1.contains(s2)) return true;
+		return false;
+	}
 }
