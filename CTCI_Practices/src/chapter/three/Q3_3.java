62a7bfa9e9054b2812740cdad0ab99c97e0c3a89
jerry_laptop <jiajiewu1988@gmail.com>
2014-04-20 11:58:57 -0700

    3.3 complete

---
+++ CTCI_Practices/src/chapter/three/Q3_3.java
@@ -0,0 +1,108 @@
+package chapter.three;
+
+/**
+ * Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
+ * Therefore, in real life, we would likely start a new stack when the previous stack
+ * exceeds some threshold. Implement a data structure SetOfStacks that mimics
+ * this. SetOfStacks should be composed of several stacks and should create a 
+ * new stack once the previous one exceeds capacity. SetOfStacks.push() and 
+ * SetOfStacks.pop() should behave identically to a single stack(that is, pop()
+ * should return the same values as it would if there were just a single stack).
+ * 
+ * FOLLOW UP
+ * 
+ * Implement a function popAt(int index) which performs a pop operation on
+ * a specific sub-stack)
+ * @author jerry
+ *
+ */
+
+public class Q3_3 {
+	public static void main(String[] args) {
+		SetOfStacks<Integer> s = new SetOfStacks<Integer>();
+		s.push(new Integer(1));
+		s.push(new Integer(1));
+		s.push(new Integer(1));
+		s.push(new Integer(1));
+		s.push(new Integer(1));
+		s.push(new Integer(2));
+		s.push(new Integer(2));
+		s.push(new Integer(2));
+		System.out.println();
+	}
+}
+
+
+class SetOfStacks<T> {
+	private Node3<Stack3<T>> currentStack;
+	private int currentStackLength; //suppose each stack contains no more than 5 elements
+	
+	public SetOfStacks() {
+		currentStack = new Node3<Stack3<T>>();
+		currentStack.data = new Stack3<T>();
+		currentStackLength = 0;
+	}
+	
+	public T push(T data) {
+		
+		if (currentStackLength < 5) {
+			currentStack.data.push(data);
+		} else {
+			Node3<Stack3<T>> newStackNode = new Node3<Stack3<T>>();
+			newStackNode.data = new Stack3<T>();
+			currentStackLength = 0;
+			newStackNode.data.push(data);
+			newStackNode.next = currentStack;
+			currentStack = newStackNode;
+		}
+		
+		currentStackLength++;
+		return data;
+	}
+	
+	public T pop() {
+		if (currentStackLength > 1) {
+			currentStackLength--;
+			return currentStack.data.pop();
+		} else {
+			if (currentStack.next == null) {
+				currentStackLength = 0;
+				return currentStack.data.pop();
+			} else {
+				T temp = currentStack.data.pop();
+				currentStack = currentStack.next;
+				currentStackLength = 5;
+				return temp;
+			}
+		}
+	}
+}
+
+class Stack3<T> {
+	Node3<T> top;
+
+	public void push(T data) {
+		Node3<T> node = new Node3<T>(data);
+		node.next = top;
+		top = node;
+	}
+	
+	public T pop() {
+		if (top == null) return null;
+		
+		T result = top.data;
+		top = top.next;
+		return result;
+	}
+}
+
+class Node3<T> {
+	T data;
+	Node3<T> next;
+	
+	public Node3() {}
+	
+	public Node3(T data) {
+		this.data = data;
+	}
+}
